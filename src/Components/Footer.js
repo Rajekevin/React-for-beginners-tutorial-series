26f0fd0ba78212fc42de228b490bf3bde13c2657
Rajekevin <rajekevin@hotmail.fr>
2020-07-12 19:34:16 +0200

    Create Menu page with tailwind css framework

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <header class="bg-gray-200 text-center p-3 absolute bottom-0 w-full">
+            totto
+            &copy; Copyright 2020
+        </header>
+    )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header class="border-b font-bold p-3">
+            totto
+        </header>
+    )
+
+}
+
+export default Header
