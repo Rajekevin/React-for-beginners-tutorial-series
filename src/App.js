a3f2e6dae01456957c930241b6ef66eca36c930c
Rajekevin <rajekevin@hotmail.fr>
2020-07-13 14:18:15 +0200

    Create route with react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import{
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Link
+} from "react-router-dom"
 import logo from './logo.svg';
 import HelloWorld from './Components/HelloWorld.js'
 import CounterExample from './Components/CounterExample'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Home from './Views/Home'
+import About from './Views/About'
 
 function App() {
   return (
     <div>
+      <Router>
        <Header />
-       <Footer />
+     
+            <div className="p-3">
+              <Switch>     
+                <Route exact path="/">       
+                      <Home />
+                </Route>
+                <Route path="/about">
+                      <About />
+                </Route>
+                <Route path="/contact-us">
+                      <About />
+                </Route>
+              </Switch>
+            </div>
+
+            <Footer />
+       </Router>
     </div>
     // <div className="App">
     //   <Header />
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,9 +3,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/free-solid-svg-icons'
 import {useTransition, animated} from 'react-spring'
 
-
-//className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
-//className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50" => mask
+import NavigationMenu from './NavigationMenu'
 
 function Navigation(){
     const [showMenu, setShowMenu] = useState(false)
@@ -47,25 +45,21 @@ function Navigation(){
         }
 
         {
-                menuTransitions.map(({ item, key, props }) => 
+            menuTransitions.map(({ item, key, props }) => 
                  item && 
                  <animated.div 
                     key={props} 
                     style={props}
                     className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
             >
-                    <span className="font-bold">
-                        The menu
-                   </span>
-                   <ul>
-                       <li>Homepage </li>
-                   </ul>
+                
+                <NavigationMenu 
+                    closeMenu={() => setShowMenu(false)}
+                />
             </animated.div>         
             )
-
         }
         </nav>
     )
 }
-
 export default Navigation
